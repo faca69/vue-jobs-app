550fcae0ffd88c380c0373c40080b6f430091e91
faca69 <fnikolov123456789@gmail.com>
2024-12-04 21:34:26 +0100

    added single post view

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,7 @@
 import HomeView from "@/views/HomeView.vue";
+import JobsView from "@/views/JobsView.vue";
+import JobView from "@/views/JobView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
@@ -8,6 +11,21 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/jobs",
+      name: "jobs",
+      component: JobsView,
+    },
+    {
+      path: "/jobs/:id",
+      name: "job",
+      component: JobView,
+    },
+    {
+      path: "/:catchAll(.*)",
+      name: "not-found",
+      component: NotFoundView,
     },
   ],
 });
